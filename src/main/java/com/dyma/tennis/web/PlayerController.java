61fd899a184e7a4970c93f6eca9e1873965262f9
Aurélien <atondoux@gmail.com>
2023-09-24 16:38:21 +0200

    :sparkles: simulated controller behavior

--- src/main/java/com/dyma/tennis/web/PlayerController.java
+++ src/main/java/com/dyma/tennis/web/PlayerController.java
@@ -1,6 +1,7 @@
 package com.dyma.tennis.web;
 
 import com.dyma.tennis.Player;
+import com.dyma.tennis.PlayerList;
 import io.swagger.v3.oas.annotations.Operation;
 import io.swagger.v3.oas.annotations.media.ArraySchema;
 import io.swagger.v3.oas.annotations.media.Content;
@@ -10,7 +11,6 @@ import io.swagger.v3.oas.annotations.responses.ApiResponses;
 import io.swagger.v3.oas.annotations.tags.Tag;
 import org.springframework.web.bind.annotation.*;
 
-import java.util.Collections;
 import java.util.List;
 
 @Tag(name = "Tennis Players API")
@@ -27,7 +27,7 @@ public class PlayerController {
     })
     @GetMapping
     public List<Player> list() {
-        return Collections.emptyList();
+        return PlayerList.ALL;
     }
 
     @Operation(summary = "Finds a player", description = "Finds a player")
@@ -39,7 +39,10 @@ public class PlayerController {
     })
     @GetMapping("{lastName}")
     public Player getByLastName(@PathVariable("lastName") String lastName) {
-        return null;
+        return PlayerList.ALL.stream()
+                .filter(player -> player.lastName().equals(lastName))
+                .findFirst()
+                .orElse(null);
     }
 
     @Operation(summary = "Creates a player", description = "Creates a player")
